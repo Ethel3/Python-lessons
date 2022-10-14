4ff529c8ab83b3923f9e4984040fa8c6c2c823c4
Ethel3 <akrasiethel4@gmail.com>
2022-10-14 15:52:06 +0000

    conditional statements

--- trial.py
+++ trial.py
@@ -1,3 +1,20 @@
 #receiving inputs
-name = input("What is your name? ")
-print("Hello " + name)
+#name = input("What is your name? ")
+#print("Hello " + name)
+
+#Types conversions
+#Note that, we have  three different data types namely; Numbers(10, 1), strings("hello", "Basket") and boolean(True, False)
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - "2004"
+# print(age)
+
+
+def number(first_num, second_num):
+    print(first_num + second_num)
+
+if 2 < 10:
+    print("less")
+else:
+    print("high")
+
+number(3, 4)
