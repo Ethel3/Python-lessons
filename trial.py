381dc4dd6d8678939894b04faf82428ced5fb08a
Ethel3 <akrasiethel4@gmail.com>
2022-10-15 02:08:10 +0000

    Arithmetic operations

--- trial.py
+++ trial.py
@@ -1,2 +1,21 @@
-school = "university of ghana"
-print("g" in school)
+#Arithmetic operators
+#addition
+print(10 + 3)
+
+#subtraction
+print(10 - 3)
+
+#multiplication
+print(10 * 3)
+
+#division with /. It gives you a floating number
+print(10 / 3)
+
+#division with //
+print(10 // 3)
+
+#modulus operator:  returns the remainder
+print(10 % 3)
+
+#exponential
+print(10 ** 3)
