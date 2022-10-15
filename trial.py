40eb173461a5e77203b032f29afeb88564dece7c
Ethel3 <akrasiethel4@gmail.com>
2022-10-15 12:30:17 +0000

    lists

--- trial.py
+++ trial.py
@@ -1,10 +1,25 @@
 #while loop
-i = 1
-while i <= 1_000:
-    print(i)
-    i = i + 1
-    
-i = 1
-while i <= 10:
-    print(i * "*")
-    i = i + 1
+# i = 1
+# while i <= 1_000:
+#     print(i)
+#     i = i + 1
+#    
+# i = 1
+# while i <= 10:
+#     print(i * "*")
+#     i = i + 1
+
+#lists
+schools = ["Legon", "UCC", "KNUST", "UEW", "Ashesi"]
+print(schools)
+
+#printing with index
+print(schools[2] or [-3])
+
+#updating in lists
+schools = ["Legon", "UCC", "KNUST", "Ashesi", "Mines"]
+schools[-1] = "IPMC"
+print(schools)
+
+#selecting ranges
+print(schools[1:3])
