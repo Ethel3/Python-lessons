b9b8f5a0d616fb9b3365046da78d7160b56a2ae0
Ethel3 <akrasiethel4@gmail.com>
2022-10-15 02:14:15 +0000

    orgmented operators

--- trial.py
+++ trial.py
@@ -1,21 +1,8 @@
 #Arithmetic operators
-#addition
-print(10 + 3)
-
-#subtraction
-print(10 - 3)
-
-#multiplication
-print(10 * 3)
-
-#division with /. It gives you a floating number
-print(10 / 3)
-
-#division with //
-print(10 // 3)
-
-#modulus operator:  returns the remainder
-print(10 % 3)
-
-#exponential
-print(10 ** 3)
+x = 20
+x = x + 5
+ 
+#orgmented 
+x += 3
+#or
+x *= 3
