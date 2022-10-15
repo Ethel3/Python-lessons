bbaf35c9e69d1a24b0b8bfce50e1ed9991136e53
Ethel3 <akrasiethel4@gmail.com>
2022-10-15 04:09:18 +0000

    receiving inputs

--- trial.py
+++ trial.py
@@ -1,10 +1,4 @@
-#  If Statements
-dollar = 8
-
-if dollar > 10:
-    print("Cedi is depreciating")
-    print("Economy is hard")
-elif dollar > 5:
-    print("We are safe")
-else:
-    print("We are growing")
+#exercise
+W = input("Weight: ")
+U = input("(K)g or (L)bs: ") 
+print("Weight" in U )
