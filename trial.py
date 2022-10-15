45eb4062087e4d77e5df898278d25cbfb04f9718
Ethel3 <akrasiethel4@gmail.com>
2022-10-15 02:27:05 +0000

    Logical operators

--- trial.py
+++ trial.py
@@ -1,7 +1,12 @@
-# operator precedence
-Z = 16 + 3 * 5
-print(Z)
+#  Logical operators
+#and operator: both 
+cost = 50
+print(cost < 70 and cost > 45)
 
-#and
-Z = (16 + 3) * 5
-print(Z)
+#or operator: at least one
+cost = 10
+print(cost < 15 or cost > 30 )
+
+#not operator: inverses
+cost = 10
+print( not cost > 39)
