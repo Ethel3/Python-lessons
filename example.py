7f4c574d2cf8e8afc165bc768767983ca818296f
Ethel3 <akrasiethel4@gmail.com>
2022-11-06 21:50:31 +0000

    conditional structures

---
+++ example.py
@@ -0,0 +1,12 @@
+#functions
+def main():
+    x, y = 10, 100
+    #conditional flow uses if, elif, else
+    if x < y:
+        result = "x is less than y"
+    print(result)
+    
+    
+    value = "one"
+if __name__== "__main__":
+    main()
