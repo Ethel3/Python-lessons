163fa01baf838efc2f8eb37eb8f2d4ea9ab25e96
Ethel3 <akrasiethel4@gmail.com>
2022-11-07 22:16:27 +0000

    match case

--- example.py
+++ example.py
@@ -11,10 +11,20 @@ def main():
     # print(result)
     
     # conditional statements  let you use "a if c else b"
-    result = "x is less than y" if x < y else "x is greater or equal to y"
+    # result = "x is less than y" if x < y else "x is greater or equal to y"
+    # print(result)
+    
+    # match-case makes it easy to compare multiple values 
+    value = "two"
+    match value:
+        case "one":
+            result = 1
+        case "two":
+            result = 2
+        case "three" | "four":
+            result = (3,4)
+        case _:
+            result= -1
     print(result)
-    
-    
-    value = "one"
 if __name__== "__main__":
     main()
