c659f9a51e1688c24a947dbeebd00c69969d0a9c
Ethel3 <akrasiethel4@gmail.com>
2022-11-07 22:11:27 +0000

    conditional statements

--- example.py
+++ example.py
@@ -1,13 +1,17 @@
 #functions
 def main():
-    x, y = 100, 100
+    x, y = 10, 100
     #conditional flow uses if, elif, else
-    if x < y:
-        result = "x is less than y"
-    elif x == y:
-        result = "x is the same as y"
-    else:
-        result = "x is greater than y"
+    # if x < y:
+    #     result = "x is less than y"
+    # elif x == y:
+    #     result = "x is the same as y"
+    # else:
+    #     result = "x is greater than y"
+    # print(result)
+    
+    # conditional statements  let you use "a if c else b"
+    result = "x is less than y" if x < y else "x is greater or equal to y"
     print(result)
     
     
