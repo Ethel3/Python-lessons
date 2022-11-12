8ea72e98ea90c5e4d9d43354af17d10d39738258
Ethel3 <akrasiethel4@gmail.com>
2022-11-12 21:13:25 +0000

    Defining classes

--- example.py
+++ example.py
@@ -34,12 +34,21 @@ class Vehicle():
     def __init__(self, bodystyle):
         self.bodystyle = bodystyle
         
+    def drive(self, speed):
+        self.mode = "driving"
+        self.speed = speed
+        
 class Car(Vehicle):
     def __init__(self, enginetype):
         super().__init__("Car")
         self.wheels = 4
         self.doors = 4
         self.enginetype = enginetype
+        
+    def drive(self, speed):
+         super().drive(speed)
+         print("Driving my", self.enginetype, "Car at", self.speed)
+    
         
 class Motorbike(Vehicle):
     def __init__(self, enginetype, hassidecar):
@@ -51,6 +60,10 @@ class Motorbike(Vehicle):
         self.doors = 0
         self.enginetype = enginetype
         
+    def drive(self, speed):
+         super().drive(speed)
+         print("Driving my", self.enginetype, "Motorbike at", self.speed)
+        
 car1 = Car("gas")
 car2 = Car("electric")
 mb1 = Motorbike("gas", True)
@@ -58,3 +71,7 @@ mb1 = Motorbike("gas", True)
 print(mb1.wheels)
 print(car1.enginetype)
 print(car2.doors)
+
+car1.drive(40)
+car2.drive(50)
+mb1.drive(70)
