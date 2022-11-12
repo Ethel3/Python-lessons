9fcc78b16ea23029d089ca9a15ff2415c2c13459
Ethel3 <akrasiethel4@gmail.com>
2022-11-12 20:59:20 +0000

    classes  updated

--- example.py
+++ example.py
@@ -37,3 +37,16 @@ class Vehicle():
 class Car(Vehicle):
     def __init__(self, enginetype):
         super().__init__("Car")
+        self.wheels = 4
+        self.doors = 4
+        self.enginetype = enginetype
+        
+class Motorbike(Vehicle):
+    def __init__(self, enginetype, hassidecar):
+        super().__init__("Motorbike")
+        if (hassidecar):
+            self.wheels = 3
+        else:
+            self.wheels = 2
+        self.doors = 0
+        self.enginetype = enginetype
