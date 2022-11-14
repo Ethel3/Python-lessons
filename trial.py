14f8f9b976345b0187df5949d286c529432f0c5a
Ethel3 <akrasiethel4@gmail.com>
2022-11-14 20:08:52 +0000

    modifying time

--- trial.py
+++ trial.py
@@ -163,13 +163,18 @@ def main():
     print(os.name)
     
     # check for item existence and type
-    print("Item exists:", str(path.exists("textfile.txt")))
-    print("Item is a file:", path.isfile("textfile.txt"))
-    print("Item is a directory:", path.isdir("textfile.txt"))
+    # print("Item exists:", str(path.exists("textfile.txt")))
+    # print("Item is a file:", path.isfile("textfile.txt"))
+    # print("Item is a directory:", path.isdir("textfile.txt"))
     
     # work with file paths
-    print("Item's path:", path.realpath("textfile.txt"))
-    print("Item's path and name:", path.split(path.realpath("textfile.txt")))
+    # print("Item's path:", path.realpath("textfile.txt"))
+    # print("Item's path and name:", path.split(path.realpath("textfile.txt")))
+    
+    # get the modification time
+    t = time.ctime(path.getmtime("textfile.txt"))
+    print(t)
+    print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
     
 if __name__== "__main__":
     main()
