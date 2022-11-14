ed8f6cc8daddc6e6947a7de1a35e8124bcd019aa
Ethel3 <akrasiethel4@gmail.com>
2022-11-14 21:45:43 +0000

    new challenge

--- challenges.py
+++ challenges.py
@@ -21,3 +21,25 @@ while (run):
         if x.isalnum():
             newstr += x
     # print("Palindrome test:", is_palindrome(newstr))
+    
+    # new challenge
+import os 
+    
+totalbytes = 0
+
+# get a list of all the files in the current directory
+dirlist = os.listdir()
+for entry in dirlist:
+    # make sure it's a file!
+if os.path.isfile(entry):
+    # add the file size to the total
+    filesize = os.path.getsize(entry)
+    totalbytes += filesize
+# create  a subdirectory called "results"
+os.mkdir("results")
+
+# create the output file 
+
+
+    
+    
