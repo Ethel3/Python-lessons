38ee77e86f322754f3c30574a0b9c3796d216af6
Ethel3 <akrasiethel4@gmail.com>
2022-11-14 19:39:32 +0000

    reading each line individually

--- trial.py
+++ trial.py
@@ -140,8 +140,11 @@ def main():
     # open the file back up and read the contents
     myfile = open("textfile.txt", "r")
     if myfile.mode == "r":
-        contents = myfile.read()
-        print(contents)
+        # contents = myfile.read()
+        # print(contents)
+        fl = myfile.readlines()
+        for x in fl:
+            print(x)
      
     
 if __name__== "__main__":
