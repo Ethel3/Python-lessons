522a8d3ac4363da5b71de949e63982133efe156c
Ethel3 <akrasiethel4@gmail.com>
2022-11-14 22:01:37 +0000

    completed challenge

--- challenges.py
+++ challenges.py
@@ -1,25 +1,25 @@
 # use the slice trick to reverse the string
-def is_palindrome(teststr):
-    if teststr == teststr[::-1]:
-        return True
-    return False
-run = True
-while (run):
-    teststr = input ("Enter string to test for palindrome or 'exit':")
+# def is_palindrome(teststr):
+#     if teststr == teststr[::-1]:
+#         return True
+#     return False
+# run = True
+# while (run):
+#     teststr = input ("Enter string to test for palindrome or 'exit':")
     
     # if the "user"types exist then quit the program
-    if teststr == exit:
-        run = False
-        break
+    # if teststr == exit:
+    #     run = False
+    #     break
     
     # convert the string to all lower case
-    teststr = teststr.lower()
+    # teststr = teststr.lower()
     
     # strip all the spaces and punctuation from the string
-    newstr = ""
-    for x in teststr:
-        if x.isalnum():
-            newstr += x
+    # newstr = ""
+    # for x in teststr:
+    #     if x.isalnum():
+    #         newstr += x
     # print("Palindrome test:", is_palindrome(newstr))
     
     # new challenge
@@ -31,15 +31,27 @@ totalbytes = 0
 dirlist = os.listdir()
 for entry in dirlist:
     # make sure it's a file!
-if os.path.isfile(entry):
-    # add the file size to the total
-    filesize = os.path.getsize(entry)
-    totalbytes += filesize
+    if os.path.isfile(entry):
+        # add the file size to the total
+        filesize = os.path.getsize(entry)
+        totalbytes += filesize
 # create  a subdirectory called "results"
 os.mkdir("results")
 
 # create the output file 
-
+resultsfile = open("results/results.txt", "w+")
+if resultsfile.mode == "w+":
+    resultsfile.write("Total bytecount:" + str(totalbytes) + "\n")
+    resultsfile.write("Files list:\n")
+    resultsfile.write("--------------\n")
+    
+    # write the results into the file
+    for entry in dirlist:
+        if os.path.isfile(entry):
+            # write the file name to the results ledger
+            resultsfile.write(entry + "\n")
+    # close file
+    resultsfile.close()
 
     
     
