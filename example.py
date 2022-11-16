462ddd0c0979e60dadf0f2806bee8763f9af6e11
Ethel3 <akrasiethel4@gmail.com>
2022-11-16 08:10:47 +0000

    manipulating xml

--- example.py
+++ example.py
@@ -77,51 +77,58 @@
 # mb1.drive(70)
 
 
-# parsing and processing html
-from html.parser import HTMLParser
+## parsing and processing html
+# from html.parser import HTMLParser
 
-paragraphs = 0
-class MyHTMLParser(HTMLParser):
-    def handle_comment(self, data):
-        print("Encountered a comment:", data)
-        pos = self.getpos()
-        print("At line:", pos[0], "position",pos[1])
+# paragraphs = 0
+# class MyHTMLParser(HTMLParser):
+#     def handle_comment(self, data):
+#         print("Encountered a comment:", data)
+#         pos = self.getpos()
+#         print("At line:", pos[0], "position",pos[1])
         
     
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
-        pos = self.getpos()
-        print("At line:", pos[0], "position",pos[1])
+#     def handle_starttag(self, tag, attrs):
+#         print("Encountered a start tag:", tag)
+#         pos = self.getpos()
+#         print("At line:", pos[0], "position",pos[1])
         
-        global paragraphs 
-        if tag == "p":
-            paragraphs +=1
+#         global paragraphs 
+#         if tag == "p":
+#             paragraphs +=1
             
             
-        if len(attrs) > 0: 
-            print("Attributes:")
-            for a in attrs:
-                print("\t", a[0],"=",a[1])
-    def handle_data(self, data):
-        if data.isspace():
-            return
-        print("Encountered text data:", data)
-        pos = self.getpos()
-        print("At line:", pos[0], "position",pos[1])
+    #     if len(attrs) > 0: 
+    #         print("Attributes:")
+    #         for a in attrs:
+    #             print("\t", a[0],"=",a[1])
+    # def handle_data(self, data):
+    #     if data.isspace():
+    #         return
+    #     print("Encountered text data:", data)
+    #     pos = self.getpos()
+    #     print("At line:", pos[0], "position",pos[1])
         
-    
+import xml.dom.minidom
+ 
 def main(): 
     # feeding the parser with html
-    parser = MyHTMLParser()
+    # parser = MyHTMLParser()
     
-    f = open("samplehtml.html")
-    if f.mode == "r":
-        contents = f.read() #read the entire file
-        parser.feed(contents)
+    # f = open("samplehtml.html")
+    # if f.mode == "r":
+        # contents = f.read() #read the entire file
+        # parser.feed(contents)
         
         
-    print("Paragraph tags:", paragraphs)
-        
+    # print("Paragraph tags:", paragraphs)
+    
+    # using the parse() function to load and parse an xml file
+    doc = xml.dom.minidom.parse(samplexml.xml)
+    
+    # print document node and the name of the first child tag
+    print(doc.nodeName)
+    print(doc.firstChild.tagName)
 if __name__== "__main__":
     main()
     
