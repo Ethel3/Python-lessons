9a103970578d2162a02be57a7f7f56d0341ada22
Ethel3 <akrasiethel4@gmail.com>
2022-11-16 08:15:53 +0000

    working with xml tags

--- example.py
+++ example.py
@@ -124,11 +124,17 @@ def main():
     # print("Paragraph tags:", paragraphs)
     
     # using the parse() function to load and parse an xml file
-    doc = xml.dom.minidom.parse(samplexml.xml)
+    doc = xml.dom.minidom.parse("samplexml.xml")
     
     # print document node and the name of the first child tag
     print(doc.nodeName)
     print(doc.firstChild.tagName)
+    
+    # get a list of XML tags from the document and print each one
+    skills = doc.getElementsByTagName("skill")
+    print(skills.length, "skills are listed")
+    for skill in skills :
+        print(skill.getAttribute("name"))
 if __name__== "__main__":
     main()
     
