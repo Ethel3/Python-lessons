c71c1956ebaef629dbac1786002912f4a30ea865
Ethel3 <akrasiethel4@gmail.com>
2023-02-16 01:23:10 +0000

    Made changes

--- example.py
+++ example.py
@@ -169,7 +169,7 @@ print ('one\nTwo\nThree')
 print("""this is a multi device""")
 # In regards to double quote strings, it must be escaped by placing a backslash before them.
 # Backslashes can be used to escape tags and arbitary unicode characters.
-print('Kofi\'s mother: He\'s not an angel.')
+print('Kofi\'s mother: He\'s not an angel')
 
 # \t represents a tab
 # string concatenation 
